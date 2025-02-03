cdb20eb6f5c85cb902e29a0dc38bf7eda43e954e
Harsh Deep Gupta <108045259+kaaliharsh@users.noreply.github.com>
2025-02-03 23:20:20 +0530

    files done

--- TweetU/TweetU/settings.py
+++ TweetU/TweetU/settings.py
@@ -135,9 +135,10 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_URL='/static/'
+STATIC_URL='static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 
 LOGIN_URL = '/accounts/login'
 LOGIN_REDIRECT_URL = '/TweetUp/'
 LOGOUT_REDIRECT_URL = '/TweetUp/'
+
