842f32aff7ec49a3f71ecdf9c3942e264fe73f87
Harsh Deep Gupta <108045259+kaaliharsh@users.noreply.github.com>
2025-02-03 23:25:16 +0530

    files done

--- TweetU/TweetU/settings.py
+++ TweetU/TweetU/settings.py
@@ -135,7 +135,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_URL='static/'
+STATIC_URL='/static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 
 LOGIN_URL = '/accounts/login'
