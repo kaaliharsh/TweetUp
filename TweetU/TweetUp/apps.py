bd32c75a75c14256399248bfdeab647c2c1d7f08
Harsh Deep Gupta <108045259+kaaliharsh@users.noreply.github.com>
2025-03-04 23:47:38 +0530

    complete code push

--- TweetU/TweetUp/apps.py
+++ TweetU/TweetUp/apps.py
@@ -4,3 +4,15 @@ from django.apps import AppConfig
 class TweetupConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "TweetUp"
+    
+    
+    
+# follow/unfollow feature code
+
+class YourAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'your_app_name'
+
+    def ready(self):
+        import your_app_name.signals
+
--- TweetU/TweetUp/models.py
+++ TweetU/TweetUp/models.py
@@ -21,3 +21,33 @@ class Tweet(models.Model):
     def get_absolute_url(self):
         from django.urls import reverse
         return reverse('tweet_detail', args=[str(self.id)])
+    
+    
+    # followers and following feature code 
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    followers = models.ManyToManyField(User, related_name="following", blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+    def follow(self, user):
+        """Follow a user"""
+        self.followers.add(user)
+
+    def unfollow(self, user):
+        """Unfollow a user"""
+        self.followers.remove(user)
+
+    def is_following(self, user):
+        """Check if the current user is following another user"""
+        return self.followers.filter(id=user.id).exists()
+
+    def followers_count(self):
+        """Return number of followers"""
+        return self.followers.count()
+
+    def following_count(self):
+        """Return number of users this user follows"""
+        return self.user.following.count()
