204f00c8a84fc5d625949932ac29ba0067f7855a
Harsh Deep Gupta <108045259+kaaliharsh@users.noreply.github.com>
2026-04-06 00:29:02 +0530

    new updated code

--- TweetU/TweetUp/apps.py
+++ TweetU/TweetUp/apps.py
@@ -7,12 +7,3 @@ class TweetupConfig(AppConfig):
     
     
     
-# follow/unfollow feature code
-
-class YourAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'your_app_name'
-
-    def ready(self):
-        import your_app_name.signals
-
